7998ad4c5ee94039f5d634993a4c4b452e094807
SagarMakhar <sagarmakhar@gmail.com>
2021-11-06 13:45:39 +0000

    Make Udfps haptic feedback optional [1/2]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I42ee0f7dd80bada4b902b107e42d00b743875bb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5417,6 +5417,13 @@ public final class Settings {
         public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
+         * Whether to play haptic feedback when touching udfps icon
+         * @hide
+         */
+        @Readable
+        public static final String UDFPS_HAPTIC_FEEDBACK = "udfps_haptic_feedback";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -92,5 +92,6 @@ public class SystemSettings {
         Settings.System.BATTERY_LIGHT_FULL_COLOR,
         Settings.System.BATTERY_LIGHT_REALLYFULL_COLOR,
         Settings.System.BATTERY_LIGHT_ENABLED,
+        Settings.System.UDFPS_HAPTIC_FEEDBACK,
     };
 }
